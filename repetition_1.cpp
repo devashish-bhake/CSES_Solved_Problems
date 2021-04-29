be7b4e94bc6b7f2feb6ad2420d12964df78eb294
devashish-bhake <devashish.b@somaiya.edu>
2021-04-29 19:55:05 +0530

    Repetition Program
    Repetition program solution for the CSES problem set

---
+++ repetition_1.cpp
@@ -0,0 +1,29 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int result = 1;
+    int i = 0;
+
+    while(i < s.size()-1)
+    {
+        int count = 1;
+
+        while(s[i] == s[i+1])
+        {
+            count++;
+            i++;
+        }
+        result = max(result, count);
+        i++;
+    }
+
+    cout << result;
+}
